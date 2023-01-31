0edc40a24cc6b0805ad436ec346fa50d45112542
Luana <luana.lorena@mail.uft.edu.br>
2023-01-31 14:10:31 -0300

    definindo cores

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from "react-router-dom";
+
+import { LightTheme } from './shared/themes';
 import { AppRoutes } from './routes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
