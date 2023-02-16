8acb56226a3651adae04d00c114721ebd3bdc645
Luana <luana.lorena@mail.uft.edu.br>
2023-02-16 14:48:10 -0300

    menu responsivo

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { MenuLateral } from './shared/components/menu-lateral/MenuLateral';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
+
+export const App = () => {
+    return (
+        <AppThemeProvider>
+            <DrawerProvider>
+                <BrowserRouter>
+                    <MenuLateral>
+                        <AppRoutes />
+                    </MenuLateral>
+                </BrowserRouter>
+            </DrawerProvider>
+        </AppThemeProvider>
+    );
+};
